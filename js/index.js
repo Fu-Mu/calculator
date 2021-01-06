63c84fcb12ff9feabc4ccb17f72bce44665005ca
LordCaster <1533096520@qq.com>
2021-01-06 12:03:31 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 var num=0;
+var bool=0;
+var result=0;
 function sr(num)
 {
 	document.getElementById('screen').value += document.getElementById(num).value;
@@ -9,5 +11,32 @@ function gl()
 }
 function js()
 {
+	result=eval(document.getElementById('screen').value);
 	document.getElementById('screen').value += "="+eval(document.getElementById('screen').value );
+	
+}
+function getresult()
+{
+	result=eval(document.getElementById('screen').value);
+}
+function change()
+{
+	bool=1;
+}
+function check1()
+{
+		if(bool==1)
+		{
+			gl();
+			bool=0;
+		}
+}
+function check2()
+{
+	if(bool==1)
+		{
+			gl();
+			bool=0;
+			document.getElementById('screen').value=result;
+		}
 }
